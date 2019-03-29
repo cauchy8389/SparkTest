47d09846a114e0f1cd283960cb28493c3d7b4162
cauchy8389 <cauchy8389@sina.com>
2019-03-29 19:50:43 +0800

    zhy comment

--- src/main/Scala/mlib/ALS_sl.scala
+++ src/main/Scala/mlib/ALS_sl.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
+import scala.collection.mutable.WrappedArray
 
 
 object als2 {
@@ -13,15 +14,56 @@ object als2 {
     System.setProperty("hadoop.home.dir", "E:\\hadoop-2.6.0-cdh5.8.5")
     val conf = new SparkConf().setMaster("local").setAppName("ALSExample")
     val spark = SparkSession.builder().config(conf).getOrCreate()
+    spark.sparkContext.setCheckpointDir("hdfs://zhy.cauchy8389.com:9000/user/zhy/spark/wd_checkpoint")
+
+    import spark.implicits._
+    val ratings = spark.read.textFile("file:///E:/download/MLlib机器学习/数据/sample_itemcf2.txt")
+      .map(_.split(',') match {
+        case Array(user, item, rate) =>
+          Rating(user.toInt, item.toInt, rate.toDouble)
+      })
+      .toDF()
 
     var modelPath = "hdfs://zhy.cauchy8389.com:9000/user/zhy/spark/mllib";
     var hadoopConf = new Configuration()
     var fs = FileSystem.get(hadoopConf)
     var dstPath = new Path(modelPath);
 
-    if(fs.exists(dstPath)){
-      val sameModel = ALSModel.load(modelPath)
+    val model = fs.exists(dstPath) match {
+      case true => ALSModel.load(modelPath)
+      case _ => {
+        val als = new ALS()
+        als.setRank(10)
+          .setMaxIter(5)
+          .setRegParam(0.01)
+          .setUserCol("user")
+          .setItemCol("product")
+          .setRatingCol("rating")
+        als.fit(ratings)
+      }
     }
+
+    val userRecs = model.recommendForAllUsers(10)
+    userRecs.foreach(row => {
+        println(row(0) + ":")
+        println(row(1))
+
+//        row(1).asInstanceOf[WrappedArray[List[(Int, Float)]]].foreach(aa =>{
+//          aa match {
+//            case List(item, rate) =>
+//              println(item, rate)
+//          }
+//        })
+//        var therow = row(1).asInstanceOf[WrappedArray[int,float]]
+//        therow.collect()
+//
+//        row(1) match {
+//          case therow:WrappedArray(Array[product,rating]) => {
+//
+//          }
+//        }
+      }
+    )
 
     var ss = 1
     //    val ss = SparkSession.builder.config(conf).getOrCreate
