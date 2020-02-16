d340f6bbb51abb43850b4e933e4b4bc2827614b7
cauchy8389 <cauchy_8389>
2020-02-16 15:27:24 +0800

    prog scala 2

---
+++ src/main/scala/progscala2/basicoop/ValueClassDollar.scala
@@ -0,0 +1,10 @@
+package progscala2.basicoop
+
+// src/main/scala/progscala3/basicoop/ValueClassDollar.sc
+
+/** 
+ * Real money classes shouldn't use Floats!
+ */
+class Dollar(val value: Float) extends AnyVal {
+  override def toString = "$%.2f".format(value)
+}
